622c03f482b301de14ea83f19bc1d0a62beafcf3
jsonsvalue <54315562+jsonsvalue@users.noreply.github.com>
2024-07-11 20:39:55 +0900

    [Bronze I] Title: 더하기 사이클, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,48 +1,24 @@
-def sum_cycle(n_str):
+def sum_cycle(n):
+    a = n//10
+    b = n % 10
+    c = (a+b)%10
     
-    if len(n_str)<2:
-        n_str = '0' + n_str
-
-    new_n_str = ''
-
-    fr, bk = int(n_str[0]), int(n_str[1]) 
-    fr_str, bk_str = n_str[0], n_str[1]
-
-    if fr + bk >= 10:
-        new_bk_str = str( fr + bk)[1]
-    else:
-        new_bk_str = str( fr + bk )
-
-    new_n_str = bk_str + new_bk_str
+    new_n = 10 * b + c
     
-    return new_n_str
+    return new_n
 
-
-n_str = input()
-if len(n_str) < 2:
-    n_str = '0' + n_str
-
-org_n_str = n_str
+n = int(input())
+org_n = n
 cycle = 0
+
 while True:
-    new_n_str = sum_cycle(n_str)
-    cycle+=1                                                                                                                                                        
-    if new_n_str == org_n_str:
-        break
-    n_str = new_n_str
+    new_n = sum_cycle(n)
+    cycle +=1
     
+    if org_n == new_n:
+        break
+    
+    n = new_n
 print(cycle)
 
-
-'''
-while n_str != sum_cycle(n_str):
-    n_str = sum_cycle(n_str)
-    sum_cycle(n_str)
     
-    cycle+=1
-    if n_str == sum_cycle(n_str):
-        break
-'''
-
-
-
